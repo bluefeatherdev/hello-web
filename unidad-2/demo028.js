e59d6e53a67939f1885bd45cc49b1013c681c6d9
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-03-30 20:17:06 -0500

    introducción a JavaScript

---
+++ unidad-2/demo028.js
@@ -0,0 +1,10 @@
+// JavaScript for demo028.html
+let btn1 = document.getElementById("btn1");
+btn1.addEventListener("click", function () {
+    alert("Hello World!");
+});
+
+function mostrar() {
+    let imagen = document.getElementById("img1");
+    imagen.style.display = "inline";
+}
