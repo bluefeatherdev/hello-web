4ff7e9ee6f3e669fccc8e0eb5dba2df74f298793
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-04-05 22:12:33 -0500

    modificar texto calculadora con botones

--- unidad-2/rubrica/script.js
+++ unidad-2/rubrica/script.js
@@ -1,2 +1,26 @@
 // JavaScript
-console.log("Hello World!");
+
+// 0. Leer input calculadora
+let calText = document.getElementById("calText");
+console.log(`calText: ${calText} ${calText.innerText}`);
+
+let calWrite = (text) => {
+    console.log(`calWrite: ${text} ${typeof text}`)
+    if (calText.innerText == "0") {
+        calText.innerText = text;
+    } else {
+        calText.innerText += text;
+    }
+}
+
+// 1. Sumar
+let sumar = (a, b) => a + b;
+
+// 2. restar
+let restar = (a, b) => a - b;
+
+// 3. multiplicar
+let multiplicar = (a, b) => a * b;
+
+// 4. dividir
+let dividir = (a, b) => a / b;
