2b7350d47ce211e663eca5a918302fdf538bbe3f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-04-05 22:56:11 -0500

    limpiar texto & eliminar último caracter calculadora

--- unidad-2/rubrica/script.js
+++ unidad-2/rubrica/script.js
@@ -1,9 +1,10 @@
 // JavaScript
 
-// 0. Leer input calculadora
+// 0. Leer texto calculadora
 let calText = document.getElementById("calText");
 console.log(`calText: ${calText} ${calText.innerText}`);
 
+// 1. Escribir texto calculadora
 let calWrite = (text) => {
     console.log(`calWrite: ${text} ${typeof text}`)
     if (calText.innerText == "0") {
@@ -13,14 +14,30 @@ let calWrite = (text) => {
     }
 }
 
-// 1. Sumar
+// 2. Limpiar texto calculadora
+let calClear = () => {
+    calText.innerText = "0";
+    console.log(`calClear: ${calText} ${calText.innerText}`);
+}
+
+// 3. Borrar últim caracter calculadora
+let calBack = () => {
+    if (calText.innerText.length == 1) {
+        calText.innerText = "0";
+    } else {
+        calText.innerText = calText.innerText.slice(0, -1);
+    }
+    console.log(`calBack: ${calText} ${calText.innerText.length} (length)`);
+}
+
+// n. Sumar
 let sumar = (a, b) => a + b;
 
-// 2. restar
+// n. restar
 let restar = (a, b) => a - b;
 
-// 3. multiplicar
+// n. multiplicar
 let multiplicar = (a, b) => a * b;
 
-// 4. dividir
+// n. dividir
 let dividir = (a, b) => a / b;
