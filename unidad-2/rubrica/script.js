d406336a4482a7284a360a7fe8c412730d737177
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-04-17 18:44:52 -0500

    rúbrica 2: prueba conexión API

--- unidad-2/rubrica/script.js
+++ unidad-2/rubrica/script.js
@@ -1,2 +1,18 @@
 // JavaScript
 console.log("Hello World! {'🌿🖥️'};");
+
+// Conexión API con Fetch (async/await)
+const apiConnection = async () => {
+    const apiURL = "https://pokeapi.co/api/v2/pokemon/";
+    try {
+        const response = await fetch(apiURL);
+        const data = await response.json();
+        console.log(data);
+        data.results.forEach(element => {
+            console.log(`${element.name}`);
+        });
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+}
+apiConnection();
