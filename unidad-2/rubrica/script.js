2532f7ad74b2b06bf12ad3604c0d430b8e6b774f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-04-17 20:14:49 -0500

    rúbrica 2: mostrar primeros 15 pokemons (prueba)

--- unidad-2/rubrica/script.js
+++ unidad-2/rubrica/script.js
@@ -36,7 +36,6 @@ fetchAllPokemons();
 // Conexión API con fetch (async/await)
 const getPokemonInfo = async () => {
     try {
-        let results = [];
         let newURL = 'https://pokeapi.co/api/v2/pokemon/1';
         const response = await fetch(newURL);
         const data = await response.json();
@@ -56,3 +55,42 @@ const getPokemonInfo = async () => {
     }
 };
 getPokemonInfo();
+
+// Mostrar los datos en el HTML
+let cardContainer = document.getElementById("cardContainer");
+const showDataInHTML = async (id) => {
+    try {
+        let newURL = `https://pokeapi.co/api/v2/pokemon/${id}`;
+        const response = await fetch(newURL);
+        const data = await response.json();
+        const pokemonId = data.id;
+        const pokemonName = data.name;
+        const pokemonAbilities = data.abilities;
+        const pokemonSprites = data.sprites;
+        
+        cardContainer.innerHTML += `
+            <div class="card">
+                <h2>${pokemonName}</h2>
+                <p>ID: ${pokemonId}</p>
+                <p>Name: ${pokemonName}</p>
+                <p>Abilities: ${pokemonAbilities.map(ability => ability.ability.name).join(', ')}</p>
+                <p>Sprites:</p>
+                <img src="${pokemonSprites.front_default}" alt="${pokemonName}">
+            </div>
+            <hr>
+        `;
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
+};
+
+// Mostrar primeros 15 pokemones
+for (let i = 1; i <= 15; i++) {
+    if (i >= 1026) {
+        console.log(`Pokemon ID: ${i + 8975}`);
+        showDataInHTML(i + 8975);
+    } else {
+        console.log(`Pokemon ID: ${i}`);
+        showDataInHTML(i);
+    }
+}
