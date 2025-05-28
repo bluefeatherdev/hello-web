1ffc9437b7b18effd6b4e0c9cc136f37235b3af2
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2025-05-28 00:29:52 -0500

    rúbrica 3: parte 2

--- unidad-3/rubrica/index.js
+++ unidad-3/rubrica/index.js
@@ -5,9 +5,10 @@ console.log("Hello World!");
 const express = require('express');
 const app = express();
 const portExpress = 3001;
+const fs = require('fs'); // 5. file system para archivos txt
 
-app.use(express.static('public')); // Middleware para servir archivos estáticos
-app.use(express.urlencoded({extended: true})); // Middleware para parsear datos de formularios POST
+app.use(express.static('public')); // 3. Middleware para servir archivos estáticos
+app.use(express.urlencoded({extended: true})); // 6. Middleware para parsear datos de formularios POST
 
 app.get('/', (req, res) => {
     res.send('¡Hola desde Express!');
@@ -21,10 +22,24 @@ app.listen(portExpress, () => {
 
 // 3. Rutas y manejo de datos
 app.post('/formulario', (req, res) => {
+    // 4. Campos del formulario requeridos
+    // Por hacer: validar de un modo más simple
     const {id, nombre, apellido, titulo, autor, editorial, año} = req.body;
     if (!id || !nombre || !apellido || !titulo || !autor || !editorial || !año) return res.redirect('/error.html');
     console.log(req.body);
-    console.log(`Datos recibidos del USUARIO: ${id} - ${nombre} - ${apellido}`);
-    console.log(`Datos recibidos del LIBRO: ${titulo} - ${autor} - ${editorial} - ${año}`);
-    res.send(`Datos recibidos: ${id} - ${nombre} - ${apellido} | ${titulo} - ${autor} - ${editorial} - ${año}`);
+    // 5. Guardar datos en un archivo txt
+    // 7. Estructura del archivo txt 
+    const text = `${id}, ${nombre}, ${apellido}, ${titulo}, ${autor}, ${editorial}, ${año}`;
+    fs.writeFile(`data/id_${id}.txt`, text, (error) => {
+        if (error) {
+            console.error('Error al escribir el archivo txt:', error);
+            return res.status(500).send('Error al escribir el archivo txt...');
+        }
+        console.log('¡Archivo txt creado exitosamente!');
+        // 8. Descargar archivo txt
+        res.download(`data/id_${id}.txt`);
+        // res.send(`¡Archivo txt creado exitosamente! | Datos: ${text}`);
+    });
+    // console.log(`Datos recibidos del LIBRO: ${titulo} - ${autor} - ${editorial} - ${año}`);
+    // res.send(`Datos recibidos: ${id} - ${nombre} - ${apellido} | ${titulo} - ${autor} - ${editorial} - ${año}`);
 })
